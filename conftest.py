c0c550df7ae2cb2f61c5090166daada254051656
Hamsa Lekha Tenneti <hamsalekha995@gmail.com>
2025-07-24 17:03:05 +1200

    Trying to re-run actions

--- conftest.py
+++ conftest.py
@@ -6,7 +6,9 @@ BASE_URL='https://qa-practice.netlify.app/'
 @pytest.fixture(scope="function")
 def browser():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False) 
+        # When running from local machine --
+        # browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch() 
         context = browser.new_context(base_url=BASE_URL)
         page = context.new_page()
         yield page
